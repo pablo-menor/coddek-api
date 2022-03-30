66a3a40c80c9a5615eb7ded02ff1fd0c99f37225
Pablo <pablomenor2001@gmail.com>
2022-03-30 20:36:53 +0200

    developer model done

---
+++ model/developer.model.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const devSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    avatar: {
+        type: String,
+        default: 'default_avatar.jpg',
+        url: String,
+    },
+    name: {
+        type: String,
+        required: false,
+    },
+    applied_offers: [
+        {
+            offerId: String,
+        }
+    ],
+    offers_saved: [
+        {
+            offerId: String,
+        }
+    ],
+    stars: {
+        type: Number,
+        default: 0,
+    },
+    cv: [
+        {
+            title: String,
+            url: String,    
+        }
+    ],
+    created_at: {
+        type: Date,
+        default: Date.now
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('Developer', devSchema)
